eb1751da86f8d68fb8060a0975b147215afa252c
Jat <jat@sinosky.org>
2022-07-15 11:14:45 +0800

    check $LANG
    Signed-off-by: Jat <jat@sinosky.org>

--- templates/fate-cluster-install/tools-install/check.sh
+++ templates/fate-cluster-install/tools-install/check.sh
@@ -7,6 +7,11 @@ then
   echo "Warning: $(whoami ) has sudo privilege?"
 fi
 
+# check utf-8
+if [[ ! "${LANG,,}" =~ utf-?8$ ]]; then
+    echo 'Warning: please set $LANG to a UTF-8 Locale. E.g.: export LANG=en_US.UTF-8'
+fi
+
 #check open files and max user process
 ulimit  -a |egrep 'open files|max user processes'|awk '{ if($1$2 == "openfiles") { if(  $4 <  64000 ) print "Warning: now open files is "$4", and need to turn up to 64000";} if( $1$2$3 == "maxuserprocesses" ) { if(  $5 < 65535 ) print "Warning: now max user processes is "$5", and need to turn up to 65535";} }'
 
