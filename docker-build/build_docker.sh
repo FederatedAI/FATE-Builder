3b887e9b6664937c76cbb9f7d1bb4e80c376cc7b
Chenlong Ma <chenlongm@vmware.com>
2023-04-17 13:19:47 +0800

    update maven version
    Signed-off-by: Chenlong Ma <chenlongm@vmware.com>

--- docker-build/build_docker.sh
+++ docker-build/build_docker.sh
@@ -108,7 +108,7 @@ packaging_fateboard() {
     fateboard_version=$(grep -E -m 1 -o "<version>(.*)</version>" ./pom.xml | tr -d '[\\-a-z<>//]' | awk -F "version" '{print $2}')
     echo "[INFO] fateboard version "${fateboard_version}
         
-    docker run --rm -u $(id -u):$(id -g) -v ${source_dir}/fateboard:/data/projects/fate/fateboard --entrypoint="" maven:3.6-jdk-8 /bin/bash -c "cd /data/projects/fate/fateboard && mvn clean package -DskipTests"
+    docker run --rm -u $(id -u):$(id -g) -v ${source_dir}/fateboard:/data/projects/fate/fateboard --entrypoint="" maven:3.8-jdk-8 /bin/bash -c "cd /data/projects/fate/fateboard && mvn clean package -DskipTests"
     mkdir -p ${package_dir}/fateboard/conf
     mkdir -p ${package_dir}/fateboard/ssh
     cp ./target/fateboard-${fateboard_version}.jar ${package_dir}/fateboard/
@@ -125,7 +125,7 @@ packaging_eggroll() {
     pull_eggroll
     cd ./eggroll
     cd ./deploy
-    docker run --rm -u $(id -u):$(id -g) -v ${source_dir}/eggroll:/data/projects/fate/eggroll --entrypoint="" maven:3.6-jdk-8 /bin/bash -c "cd /data/projects/fate/eggroll/deploy && bash auto-packaging.sh"
+    docker run --rm -u $(id -u):$(id -g) -v ${source_dir}/eggroll:/data/projects/fate/eggroll --entrypoint="" maven:3.8-jdk-8 /bin/bash -c "cd /data/projects/fate/eggroll/deploy && bash auto-packaging.sh"
     mkdir -p ${package_dir}/eggroll
     mv ${source_dir}/eggroll/eggroll.tar.gz ${package_dir}/eggroll/
     cd ${package_dir}/eggroll/
